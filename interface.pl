a820a8b9c6cb02e006fdc304ecfb9cc83cae94cc
Ahcamn <Ahcamn.ivalice@gmail.com>
2017-06-01 19:08:02 +0200

    Update interface.pl

--- interface.pl
+++ interface.pl
@@ -1,10 +1,20 @@
 
+% Permet de sauvegarder le board
+:- dynamic board/1.
+
+init_board([0, 0, 0, 0, -1, 0, 0, 0, 0]).
 
 start(0) :- 
   level(LVL), 
-  
+  init_board(B),
+  play(B, LVL).
 
 start(1) :- 
-  levelIA(LVL1,LVL2),
+  levelIA(LVL1, LVL2),
+  init_board(B),
+  playIA(B, LVL1, LVL2).
 
 start(_) :- !.
+
+
+play(B, LVL) :-
