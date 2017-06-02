6c313a98e15b77b6f862ef8621a09219f0872b1e
Ahcamn <Ahcamn.ivalice@gmail.com>
2017-06-02 02:56:10 +0200

    Update regles_jeu.pl

--- regles_jeu.pl
+++ regles_jeu.pl
@@ -7,18 +7,18 @@
 
 %Conditions de victoires
 %3 pions sur une ligne
-win(Plr, [Plr,Plr,Plr,_,_,_,_,_,_]) :- Plr != 0, !.
-win(Plr, [_,_,_,Plr,Plr,Plr,_,_,_]) :- Plr != 0, !.
-win(Plr, [_,_,_,_,_,_,Plr,Plr,Plr]) :- Plr != 0, !.
+win(Plr, [Plr,Plr,Plr,_,_,_,_,_,_]) :- Plr \= 0, !.
+win(Plr, [_,_,_,Plr,Plr,Plr,_,_,_]) :- Plr \= 0, !.
+win(Plr, [_,_,_,_,_,_,Plr,Plr,Plr]) :- Plr \= 0, !.
 
 %3 pions sur une colonne
-win(Plr, [Plr,_,_,Plr,_,_,Plr,_,_]) :- Plr != 0, !.
-win(Plr, [_,Plr,_,_,Plr,_,_,Plr,_]) :- Plr != 0, !.
-win(Plr, [_,_,Plr,_,_,Plr,_,_,Plr]) :- Plr != 0, !.
+win(Plr, [Plr,_,_,Plr,_,_,Plr,_,_]) :- Plr \= 0, !.
+win(Plr, [_,Plr,_,_,Plr,_,_,Plr,_]) :- Plr \= 0, !.
+win(Plr, [_,_,Plr,_,_,Plr,_,_,Plr]) :- Plr \= 0, !.
 
 %3 pions sur une diagonale
-win(Plr, [Plr,_,_,_,Plr,_,_,_,Plr]) :- Plr != 0, !.
-win(Plr, [_,_,Plr,_,Plr,_,Plr,_,_]) :- Plr != 0, !.
+win(Plr, [Plr,_,_,_,Plr,_,_,_,Plr]) :- Plr \= 0, !.
+win(Plr, [_,_,Plr,_,Plr,_,Plr,_,_]) :- Plr \= 0, !.
 
 
 %position de la case déplaçable en fonction de la case vide
