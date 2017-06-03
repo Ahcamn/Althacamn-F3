e7b6b8b98ea9bb8b8cb80e9cba8512f9d057ecf3
Ahcamn <Ahcamn.ivalice@gmail.com>
2017-06-03 20:11:43 +0200

    Update main.pl

--- main.pl
+++ main.pl
@@ -11,9 +11,17 @@ main :-
   writeln('\t1 - IA Vs. IA'),
   writeln('\t2 - Joueur Vs. Joueur'),
   writeln('\t3 - Quitter'),
-  scan_choice(C),
+  menu_choice(C),
   tty_clear,
   start(C).
+  
+menu_choice(C) :-
+    read(C),
+    integer(C),
+    between(0, 3, C), !.  
+menu_choice(C) :-
+    writeln('Erreur : Le choix doit être 0, 1, 2 ou 3 !'),
+    menu_choice(C).
   
   % Lance le menu
 :- set_prolog_stack(global, limit(2*10**9)), main.
