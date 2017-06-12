e8584917c26f44d48cd9cff175735bf54a0f6205
Altharos <pierrick.graf@outlook.com>
2017-06-12 21:49:54 +0200

    Update new_ia.pl

--- new_ia.pl
+++ new_ia.pl
@@ -1,8 +1,3 @@
-% TO DO
-% - eval
-% - alpha_beta
-% - élaguer
-
 :- module(mod_ia, [evaluation_board/3, alpha_beta/7]).
 :- use_module('regles_jeu.pl').
 :- use_module('interface.pl').
@@ -16,11 +11,11 @@
 % du signe de Val (si positif avantage pour le joueur actuel, sinon pour
 % l'adversaire).
 evaluation_board(B, Plr, 100) :-
-	win(Plr, B).
+	win(Plr, B),!.
 evaluation_board(B, Plr, -100) :-
 	get_opponent(Plr, Opp),
-	win(Opp, B).
-evalutaion_board(B, Plr, Val) :- 
+	win(Opp, B),!.
+evaluation_board(B, Plr, Val) :- 
 	get_opponent(Plr, Opp),
 	evaluation_value(Plr, B, ValPlr),
 	evaluation_value(Opp, B, ValOpp),
@@ -30,33 +25,24 @@ evalutaion_board(B, Plr, Val) :-
 % evaluation_value(+Plr, +B, ?Val)
 % Retourne une valeur en fonction du plateau B correspondant aux "points" du joueur.
 evaluation_value(Plr, B, Val) :-
-	foreach(row(R), points(R, Plr, B, Pts1)),
-	foreach(column(C), points(C, Plr, B, Pts2)),
-	foreach(diagonal(D), points(D, Plr, B, Pts3)),
-	Val is Pts1 + Pts2 + Pts3.
+	row(B, 1, R1), points(R1, Plr, B, PtsR1),
+	row(B, 2, R2), points(R2, Plr, B, PtsR2),
+	row(B, 3, R3), points(R3, Plr, B, PtsR3),
+	column(B, 1, C1), points(C1, Plr, B, PtsC1),
+	column(B, 2, C2), points(C2, Plr, B, PtsC2),
+	column(B, 3, C3), points(C3, Plr, B, PtsC3),
+	diagonal(B, 1, D1), points(D1, Plr, B, PtsD1),
+	diagonal(B, 2, D2), points(D2, Plr, B, PtsD2),
+	Val is PtsR1 + PtsR2 + PtsR3 + PtsC1 + PtsC2 + PtsC3 + PtsD1 + PtsD2.
 	
 	
 % points(+L, +Plr, +B, ?Pts)
 % Compte le nombre de pions du joueur Plr dans une liste et attribue des points
 % en fonction de ce nombre.
 points(L, Plr, B, Pts) :-
-	board_value(L, B, BL),
-	include(=:=(Plr), BL, PPlr),
+	include(=:=(Plr), L, PPlr),
 	length(PPlr, X),
 	pts_value(X, Pts).
-
-
-% Récupère la "valeur" des éléments situés sur les indexs présents dans L du plateau.
-% (par exemple si L représente la première colonne, on récupère les valeurs du 
-% plateau pour les indexs 0, 3, 6). Cela permet de connaitre le nombre de pions d'un 
-% joueur sur le plateau.	
-board_value(L, B, [Y1, Y2, Y3]) :-
-	nth0(0, L, X1),
-	nth0(X1, B, Y1),
-	nth0(1, L, X2),
-	nth0(X2, B, Y2),
-	nth0(2, L, X3),
-	nth0(X3, L, Y3).
 
 	
 % pts_value(+X, ?Pts)
@@ -70,49 +56,49 @@ pts_value(3, 100).
 % alpha_beta(+Plr, +B, +Depth, +Alpha, +Beta, ?Move, ?Value)
 % Algorithme d'élagage alpha-beta, depth représentant la profondeur 
 % de l'algorithme et value le score évalué pour un plateau donné.
-alpha_beta(Plr, B, 0, _, _, _, Value) :-
-	writeln('Depth = 0'),!,
+alpha_beta(Plr, B, 0, Alpha, Beta, Move, Value) :-
 	evaluation_board(B, Plr, Value).
-alpha_beta(Plr, B, _, _, _, _, 100) :-
-	evaluation_board(B, Plr, 100).
-alpha_beta(Plr, B, _, _, _, _, -100) :-
-	evaluation_board(B, Plr, -100).
-alpha_beta(Plr, B, Depth, Alpha, Beta, Move, _) :-
+%alpha_beta(Plr, B, _, _, _, _, 100) :-
+%	evaluation_board(B, Plr, 100).
+%alpha_beta(Plr, B, _, _, _, _, -100) :-
+%	evaluation_board(B, Plr, -100).
+alpha_beta(Plr, B, Depth, Alpha, Beta, Move, Value) :-
 	findall(X, move(Plr, B, X, _), Moves), 
 	Alpha1 is -Beta,
 	Beta1 is -Alpha,
-	write('alpha_beta / Depth = '), write(Depth), nl,
+	%write('alpha_beta / Depth = '), write(Depth), nl,
 	find_best(Plr, B, Depth, Alpha1, Beta1, Moves, Move).
 
 
 % find_best(+Plr, +B, +Depth, +Alpha, +Beta, +Moves, ?BestMove)
 % Trouve le meilleur coup à jouer.	
-find_best(_, _, _, _, _, [], _).	
 find_best(Plr, B, Depth, Alpha, Beta, [Move|RMoves], BestMove) :-
 	move(Plr, B, Move, NewB),
 	get_opponent(Plr, Opp),
-	writeln(NewB),
-    	write('Alpha : '), write(Alpha), nl,
-    	write('Beta : '), write(Beta), nl,
-	writeln('-----------'),
+	%writeln('-----------'),
+	%writeln(NewB),
+    %write('Alpha : '), write(Alpha), nl,
+    %write('Beta : '), write(Beta), nl,
 	Depth1 is Depth-1,
 	alpha_beta(Opp, NewB, Depth1, Alpha, Beta, Move, Value),
 	Value1 is -Value,
-	alpha_test(Plr, B, Depth1, Alpha, Beta, [Move|RMoves], BestMove, Value1).
-	
+	alpha_test(Plr, B, Depth, Alpha, Beta, Move, RMoves, BestMove, Value1).
 	
 % Effectue l'élagage de l'arbre en fonction d'alpha et beta (et de la valeur 
 % de score obtenue). On arrête la recherche lorsque Alpha >= Beta, lorsque
 % Value >= Alpha on appelle find_best avec Alpha = Value et on remplace BestMove
 % par le move actuel. Dans les autres cas on appelle juste find_best avec 
 % les mêmes paramètres qu avant. 
-alpha_test(_, _, _, Alpha, Beta, _, _, _):-
-	writeln('alpha_test 1'),
-	Alpha >= Beta.
-alpha_test(Plr, B, Depth, Alpha, Beta, [Move|RMoves], Move, Value):-
-	writeln('alpha_test 2'),
-	Value >= Alpha,
+alpha_test(_, _, _, Alpha, Beta,_, _, _, _):-
+	Alpha >= Beta,!.
+	
+alpha_test(Plr, B, Depth, Alpha, Beta, Move, [], Move, Value):-
+	Value >= Alpha,!.
+alpha_test(Plr, B, Depth, Alpha, Beta, _, [], BestMove, Value).
+	
+alpha_test(Plr, B, Depth, Alpha, Beta, Move, RMoves, Move, Value):-
+	Value >= Alpha,!,
 	find_best(Plr, B, Depth, Value, Beta, RMoves, Move).
-alpha_test(Plr, B, Depth, Alpha, Beta, [_|RMoves], BestMove, _):-
-	writeln('alpha_test 3'),
+
+alpha_test(Plr, B, Depth, Alpha, Beta, _, RMoves, BestMove, Value):-
 	find_best(Plr, B, Depth, Alpha, Beta, RMoves, BestMove).
